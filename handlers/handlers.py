50c15c389e7b5239534849fe1b0e421bd743d31d
polyakov_ks@smart-transport.ru <polyakov_ks>
2024-07-08 21:35:40 +0500

    init

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,48 +1,80 @@
-from aiogram import F, Router
-from aiogram.filters import Command, CommandStart
+from aiogram import Router
+from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import Message, CallbackQuery
 
-import keyboards.keyboards as kb
-
+from keyboards.keyboards import kb_salary
 
 router = Router()
 
 
+def salary(salary: int, month: str):
+    data = {
+        'Декабрь': {'days_to_avans': 10, 'work_day': 20},
+        'Январь': {'days_to_avans': 5, 'work_day': 17},
+        'Февраль': {'days_to_avans': 11, 'work_day': 20},
+
+        'Март': {'days_to_avans': 10, 'work_day': 20},
+        'Апрель': {'days_to_avans': 11, 'work_day': 21},
+        'Май': {'days_to_avans': 8, 'work_day': 20},
+
+        'Июнь': {'days_to_avans': 9, 'work_day': 19},
+        'Июль': {'days_to_avans': 11, 'work_day': 23},
+        'Август': {'days_to_avans': 11, 'work_day': 22},
+
+        'Сентябрь': {'days_to_avans': 10, 'work_day': 21},
+        'Октябрь': {'days_to_avans': 11, 'work_day': 23},
+        'Ноябрь': {'days_to_avans': 10, 'work_day': 21},
+    }
+
+    month = data.get(month.capitalize())
+    wd = month.get('work_day')
+    dta = month.get('days_to_avans')
+
+    avans = (int(salary) / wd) * dta
+    zepeshka = int(salary) - avans
+    return round(avans), round(zepeshka)
+
+
+# Обработчик команды /count_salary
 class Reg(StatesGroup):
-    name = State()
-    number = State()
+    confirm = State()
+    salary = State()
+    month = State()
 
 
-@router.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.reply(f'Привет, {message.from_user.full_name}!', reply_markup=kb.main)
-
-
-@router.callback_query(F.data == 'catalog')
-async def catalog(callback: CallbackQuery):
-    await callback.answer('Вы выбрали каталог')
-    await callback.message.edit_text('Привет!', reply_markup=await kb.inline_cars())
-
-
-@router.message(Command('reg'))
+@router.message(Command('count_salary'))
 async def reg_one(message: Message, state: FSMContext):
-    await state.set_state(Reg.name)
-    await message.answer('Введите Ваше имя')
+    await state.set_state(Reg.confirm)
+    await message.reply('Считаем зарплату? 🤔', reply_markup=kb_salary['confirm'])
 
 
-@router.message(Reg.name)
-async def reg_two(message: Message, state: FSMContext):
-    await state.update_data(name=message.text)
-    await state.set_state(Reg.number)
-    await message.answer('Введите номер телефона')
+# Обработчик ответа Да/Нет на подтверждение
+@router.callback_query(lambda c: c.data in ('yes', 'no'))
+async def process_confirm(callback: CallbackQuery, state: FSMContext):
+    await callback.answer('')
+    if callback.data == 'yes':
+        await state.set_state(Reg.salary)
+        await callback.message.answer('Какая у Вас месячная зарплата?', reply_markup=kb_salary['salary'])
+    else:
+        await callback.message.answer('Хорошо, может быть в другой раз! 👋')
+        await state.clear()
 
 
-@router.message(Reg.number)
-async def two_three(message: Message, state: FSMContext):
-    await state.update_data(number=message.text)
+# Обработчик выбора зарплаты
+@router.callback_query(lambda c: c.data in ('60000', '70000'))
+async def process_salary(callback: CallbackQuery, state: FSMContext):
+    await callback.answer('')
+    await state.update_data(salary=callback.data)
+    await state.set_state(Reg.month)
+    await callback.message.answer('За какой месяц считаем зарплату?', reply_markup=kb_salary['month'])
+
+
+@router.callback_query(Reg.month)
+async def two_three(callback: CallbackQuery, state: FSMContext):
+    await state.update_data(month=callback.data)
     data = await state.get_data()
-    await message.answer(f'Спасибо, регистрация завершена.\nИмя: {data["name"]}\nНомер: {data["number"]}')
-    await state.clear()
-    print(data)
+    sal = salary(data.get('salary'), data.get('month'))
+    await callback.message.answer(f'Месяц: {data["month"]}\nОбщая зарплата: {data["salary"]}\nАванс и Зарплата:{sal}')
+    # await state.clear()
--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,6 +1,4 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-
 
 main = InlineKeyboardMarkup(
     inline_keyboard=[
@@ -12,17 +10,33 @@ main = InlineKeyboardMarkup(
     ]
 )
 
-settings = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text='YouTube', url='https://youtube.com')],
-    ]
-)
-
-cars = ['lada', 'toyota', 'bmw', 'ford']
-
-
-async def inline_cars():
-    keyboard = InlineKeyboardBuilder()
-    for car in cars:
-        keyboard.add(InlineKeyboardButton(text=car, callback_data=f'car_{car}'))
-    return keyboard.adjust(2).as_markup()
+kb_salary = {
+    'confirm': InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text='Да', callback_data='yes'),
+             InlineKeyboardButton(text='Нет', callback_data='no')],
+        ]
+    ),
+    'salary': InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text='60 000', callback_data='60000')],
+            [InlineKeyboardButton(text='70 000', callback_data='70000')],
+        ]
+    ),
+    'month': InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text='Январь', callback_data='Январь')],
+            [InlineKeyboardButton(text='Февраль', callback_data='Февраль')],
+            [InlineKeyboardButton(text='Март', callback_data='Март')],
+            [InlineKeyboardButton(text='Апрель', callback_data='Апрель')],
+            [InlineKeyboardButton(text='Май', callback_data='Май')],
+            [InlineKeyboardButton(text='Июнь', callback_data='Июнь')],
+            [InlineKeyboardButton(text='Июль', callback_data='Июль')],
+            [InlineKeyboardButton(text='Август', callback_data='Август')],
+            [InlineKeyboardButton(text='Сентябрь', callback_data='Сентябрь')],
+            [InlineKeyboardButton(text='Октябрь', callback_data='Октябрь')],
+            [InlineKeyboardButton(text='Ноябрь', callback_data='Ноябрь')],
+            [InlineKeyboardButton(text='Декабрь', callback_data='Декабрь')],
+        ]
+    )
+}
